acd3a88f96e2aa27f19e8f7a34c0bf2020c04eaf
Farsheed Ashouri <rodmena@me.com>
2019-07-19 14:52:57 +0430

    added extra analytical data to saved historical

--- lib/farsheed_trader/external_data.ex
+++ lib/farsheed_trader/external_data.ex
@@ -9,10 +9,59 @@ defmodule FarTrader.ExternalData do
   # import Ecto.Query, only: [from: 2]
   #
   #
+  #
+  def get_all_historical_data do
+    FarTrader.Stock |> FarTrader.Repo.all |> Enum.map(fn s ->
+      Rihanna.enqueue({__MODULE__, :save_trade_history, [s]})
+      Rihanna.enqueue({__MODULE__, :save_symbol_ext_data, [s]})
+    end)
+  end
+
+
+
+  @doc """
+  		saves symbol extra analytical data for later usage
+  """
+  def save_symbol_ext_data(stock) do
+    epoch = DateTime.utc_now() |> DateTime.to_unix(:millisecond)
+    basedir = :code.priv_dir(:farsheed_trader)
+
+    savedir =
+      "#{basedir}/historical_data/sahamyab.com/api/proxy/symbol/getSymbolExtData/#{
+        stock.isin
+      }"
+
+    File.mkdir(savedir)
+
+    headers = [
+      {"user-agent",
+       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36 OPR/62.0.3331.66"},
+      {"accept", "application/json"}
+    ]
+
+    url =
+      "https://www.sahamyab.com/api/proxy/symbol/getSymbolExtData?v=0.1&code=#{stock.fa_symbol}&stockWatch=1&_=#{epoch}"
+      |> URI.encode()
+
+    case Utils.http_get(url, headers) do
+      %HTTPoison.Response{:status_code => 200, :body => body} ->
+        # {unixtime, first_traded_price, highest_price, lowest_price, last_traded_price, volume}
+        rawfilepath = "#{savedir}/raw.json"
+        :ok = File.write(rawfilepath, body)
+
+      %HTTPoison.Response{:status_code => _} ->
+        :error
+    end
+  end
+
+
+
+
   @doc """
   		saves trade history to priv/historical folder for later analysis
   """
   def save_trade_history(stock) do
+    epoch = DateTime.utc_now() |> DateTime.to_unix(:millisecond)
     basedir = :code.priv_dir(:farsheed_trader)
 
     savedir =
@@ -29,7 +78,7 @@ defmodule FarTrader.ExternalData do
     ]
 
     url =
-      "https://www.sahamyab.com/api/proxy/symbol/symbolCandleChartData?namad=#{stock.fa_symbol}&type=all"
+      "https://www.sahamyab.com/api/proxy/symbol/symbolCandleChartData?namad=#{stock.fa_symbol}&type=all&_=#{epoch}"
       |> URI.encode()
 
     case Utils.http_get(url, headers) do
